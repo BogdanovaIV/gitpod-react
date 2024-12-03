e770ed65a39da217201fd62eb63e0fb9df2f4df7
Irina Bogdanova <irinabogdanova261@gmail.com>
2024-12-03 09:41:46 +0000

    Usinf stateful class component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import FunctionalGreedingWithProps from './components/FunctionalGreedingWithProps';
+import StatefulGreeding from './components/StatefulGreeding';
 
 
 function App() {
   return (
     <div className="App">
       <FunctionalGreedingWithProps greeding = "nice to meet you!" name = "Mike" age = "32"/>
+      <StatefulGreeding greeding = "from the stateful class component!" name = "Mike" age = "32"/>
     </div>
   );
 }
